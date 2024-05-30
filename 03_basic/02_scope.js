74f0291575a31b9d8fdea80007f437bf5bb3beac
razahussain976 <160427538+razahussain976@users.noreply.github.com>
2024-05-30 22:20:50 +0000

    global  & block scope

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -5,7 +5,7 @@ if (true) {
     let a = 10;
 const b = 20;
 var c = 30;
-console.log("Inner", a);
+// console.log("Inner", a);
 }
 
 
@@ -27,14 +27,44 @@ function one (){
 
 // one()
 
-
+const fullName = "Raza Mubeen";
 
 if(true){
     const username = "Raza";
+    // console.log(fullName);
     if(username === "Raza"){
-        
+        const website = " Youtube";
+        // console.log(username + website);
+        // console.log(fullName);
     }
+    // console.log(website);
 }
+// console.log(username);
+
+
+// +++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++
+console.log(addOne(5));
+function addOne (num){
+      
+    return num + 1
+}
+
+
+// console.log(addTwo(6));
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(6)
+
+
+
+
+
+
+
+
+
 
 
 
