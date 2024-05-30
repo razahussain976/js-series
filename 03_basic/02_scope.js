2bbcf1b280e0aa2c48eb01e41821db90ee42a142
razahussain976 <160427538+razahussain976@users.noreply.github.com>
2024-05-30 16:04:09 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,47 @@
+var c = 200;
+let a = 300;
+
+if (true) {
+    let a = 10;
+const b = 20;
+var c = 30;
+console.log("Inner", a);
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one (){
+    const username = "Raza";
+
+    function two (){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website)
+    two()
+}
+
+// one()
+
+
+
+if(true){
+    const username = "Raza";
+    if(username === "Raza"){
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
