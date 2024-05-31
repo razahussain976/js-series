2c03892c011cc40cf219daa12d92a40828b393b1
razahussain976 <160427538+razahussain976@users.noreply.github.com>
2024-05-31 13:56:17 +0000

    iifi

---
+++ 03_basic/04_iifi.js
@@ -0,0 +1,18 @@
+// immediately invoked function expression (IIFI)
+
+
+(function username (){
+    //name iifi
+    console.log(`DB connected`);
+})();
+
+((name) => { 
+    console.log(`DB connected two ${name}`)
+})("Raza")
+
+
+
+
+
+
+
