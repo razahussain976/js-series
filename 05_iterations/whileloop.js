757ebbed1407eee30de92871c6353027c08c01ce
razahussain976 <160427538+razahussain976@users.noreply.github.com>
2024-06-04 21:27:54 +0000

    while&dowhile loop

---
+++ 05_iterations/whileloop.js
@@ -0,0 +1,22 @@
+let myArray = 0
+
+while (myArray < 10) {
+    // console.log(myArray);
+
+    myArray = myArray + 2
+}
+const Array1 = ["flash", "batman", "superman"]
+
+let arr = 0;
+
+while (arr < Array1.length) {
+    // console.log(Array1[arr]);
+arr++
+
+}
+    let i = 11
+
+    do {
+        console.log(i);
+        i++
+    } while (i < 10);
