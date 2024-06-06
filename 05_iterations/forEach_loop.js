b7f92f7f960270625fc24c5c78a7f54bce2e0615
razahussain976 <160427538+razahussain976@users.noreply.github.com>
2024-06-06 22:13:13 +0000

    forEach loop

---
+++ 05_iterations/forEach_loop.js
@@ -0,0 +1,50 @@
+const coding = ["js", "rb", "py", "java", "cpp"];
+
+// coding.forEach(function (val) {
+//     console.log(val);
+// })
+
+// coding.forEach((item) => {
+// console.log(item);
+// })
+
+function printme(item) {
+    console.log(item);
+}
+
+
+// coding.forEach((printme))
+
+
+
+// coding.forEach((item,index,arr) => {
+// console.log(item , index , arr);
+// })
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+
+    {
+        languageName: "java",
+        languageFileName: "java"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+})
+
+
+
+
+
+
