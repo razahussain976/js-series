02fc065f02f935a024f8e3e9fda260677b8ea882
razahussain976 <160427538+razahussain976@users.noreply.github.com>
2024-06-06 21:48:57 +0000

    forIn loop

---
+++ 05_iterations/forin_loop.js
@@ -0,0 +1,32 @@
+const myObject = {
+    js: "javascript",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);    
+};
+
+const programming = ["js", "rb", "py", "java", "cpp"];
+
+for (const key in programming) {
+    // console.log(programming[key]);
+
+    }
+
+
+    
+const map = new Map()
+
+map.set('pk', "Pakistan")
+map.set('usa', "United State Of America")
+
+
+for (const key in map) {
+
+    // console.log(key);    
+    
+ }
+    
